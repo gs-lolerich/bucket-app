170aa6f01b1a22bcb5413e5e2c9920de2638a1f3
Lauren Olerich <lolerich@gainsight.com>
2025-10-02 16:25:53 -0700

    Initial commit

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+export function getSupabaseConfig() {
+  return {
+    url: 'https://zxjclmnxjavejrhfkuuj.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4amNsbW54amF2ZWpyaGZrdXVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzODQ0MzIsImV4cCI6MjA3NDk2MDQzMn0.EdNZQOYypn1ZrH6iT15usXCruwkMFuCNu6n-dUDpALE'
+  };
+}
